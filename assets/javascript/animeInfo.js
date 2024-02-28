ec9bf783d54efb3f4efd8eea832d163bd7fcdfce
ShaunZap <sjbzappy@gmail.com>
2024-02-28 16:01:06 +0530

    updated login, added sign up page, implemented session storage

--- assets/javascript/animeInfo.js
+++ assets/javascript/animeInfo.js
@@ -104,9 +104,9 @@ async function getEpisodeList(link) {
     });
     const pagination = document.getElementById("pagination-container");
     pagination.innerHTML = `
-        <button id="previous" onclick="previous()">Previous</button>
+        <button id="previous" onclick="previous()"><<</button>
             <div id="count">Page: ${currentPage}</div>
-        <button id="next" onclick="next()">Next</button>
+        <button id="next" onclick="next()">>></button>
     `;
 }
 
--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,28 +1,65 @@
-const login = document.getElementById("login");
-const tempUser = 'shaun';
-const tempPass = 'shaun@123';
-const url = `../../pages/anime.html`;
+const defaultUser = 'admin';
+const defaultPassword = 'admin';
+const animeUrl = `../../pages/anime.html`;
+const loginurl = `../../index.html`;
 const notification = document.getElementById("notification");
 const errorMessage = 'Incorrect username or password. Please try again.';
+const registerError = 'Passwords don\'t match or fields are empty. Fill all fields.';
 
-login.addEventListener('click', function(){
-    loginFunction();
-});
+function addKeyPressListener() {
+    document.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            loginFunction();
+            registerFuntion();
+        }
+    });
+}
 
-document.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
+if (document.getElementById('login')) {
+    const login = document.getElementById("login");
+    login.addEventListener('click', function () {
         loginFunction();
-    }
-});
+    });
+    addKeyPressListener();
+}
+
+if (document.getElementById('register')) {
+    const register = document.getElementById('register');
+    register.addEventListener('click', function () {
+        registerFuntion();
+        // console.log('clicked');
+    });
+    addKeyPressListener();
+}
 
 function loginFunction() {
     const username = document.getElementById('username').value;
     const userPassword = document.getElementById('password').value;
-    if(username == tempUser && userPassword == tempPass) {
-        history.replaceState(null, '', window.location.href = url);
+    const tempUser = sessionStorage.getItem('tempUser');
+    const tempPass = sessionStorage.getItem('tempPass');
+    // console.log("username:",username,"password:",userPassword,'tempuser:',tempUser,'tempPass',tempPass);
+    if ((username == tempUser && userPassword == tempPass) || (username == defaultUser && userPassword == defaultPassword)) {
+        history.replaceState(null, '', window.location.href = animeUrl);
     } else {
         showNotification(errorMessage);
     }
+}
+
+function registerFuntion() {
+    const userR = document.getElementById('usernameR').value;
+    const passR = document.getElementById('passwordR').value;
+    const email = document.getElementById('email').value;
+    const confirmPass = document.getElementById('confirm-passwordR').value;
+
+    sessionStorage.setItem('tempUser', userR);
+    sessionStorage.setItem('tempPass', passR);
+    // console.log('tempuser:',tempUser,'tempPass',tempPass);
+    if (passR == confirmPass && passR != ' ' && email != ''){
+    // console.log('tempuser:',tempUser,'tempPass',tempPass);
+        window.location.href = loginurl;
+    }
+    else
+        showNotification(registerError);
 }
 
 function showNotification(message) {
